00eb466f53703a39038ed1cd281f8ea1159da5c1
Rupali Palikhe <79547654+rupali-p@users.noreply.github.com>
2023-04-05 13:00:56 +1000

    using '?' operator for sake of task but should not be used irl

--- name.js
+++ name.js
@@ -1,5 +1,5 @@
 'use strict';
-let userName = prompt("what is your name?", "")
+let userName = prompt("what is your name?", "");
 if (!confirm(`your name is ${userName}`))
-    userName = prompt("what is your real name?", "")
-alert(`awesome! hello ${userName}`)
+    userName = prompt("what is your real name?", "");
+alert(`awesome! hello ${userName}`);
